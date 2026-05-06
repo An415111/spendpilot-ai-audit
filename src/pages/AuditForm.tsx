719e775a7580572ee583eec67d6b025808f4c53d
music_project <ankitgupta37937@gmail.com>
2026-05-06 22:17:25 +0530

    feat: setup routing and initial pages

---
+++ src/pages/AuditForm.tsx
@@ -0,0 +1,9 @@
+const AuditForm = () => {
+  return (
+    <div>
+      <h1>Audit Form</h1>
+    </div>
+  );
+};
+
+export default AuditForm;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>SpendPilot</h1>
+    </div>
+  );
+};
+
+export default Home;
